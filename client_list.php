9545b1ae1b087a2dc766421a89154509d7c99f33
Junmar Rosario <junmarr30@gmail.com>
2024-04-03 16:49:08 +0800

    updating table on 2 form

--- client_list.php
+++ client_list.php
@@ -58,13 +58,13 @@
           </div>
   </div>
 
-      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-      <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
       <script>
           $(document).ready(function () {
               $('#clientTable').DataTable();
           });
       </script>
 </body>
-
-  </html>
+</html>
--- new_prospect.php
+++ new_prospect.php
@@ -16,13 +16,16 @@
     <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <p class="text-2xl font-bold mb-4">New Prospect</p>
       <div class="relative ">
-        <div class="absolute top-0 right-10 h-16 w-16 flex items-center justify-center">
-          <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add Prospect</button>
-        </div>
-      </div>
+      <!-- <div class="absolute top-0 right-10 h-16 w-16 flex items-center justify-center">
+        <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+        Add prospect
+        </button>
+      </div> -->
+    </div>
+
+    <button style="margin-left: 85%; margin-bottom: 1%;" type="button" class="btn btn-success  justify-content-center" data-bs-toggle="modal" data-bs-target="#addNewProspect">Add Record</button>
 
     <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-    <h2 class="text-2xl font-bold mb-4">Example Datatable</h2>
     <table id="example" class="table-auto w-full">
         <thead>
           <tr>
@@ -61,7 +64,137 @@
   </div>
 </div>
 
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+
+
+    <!-- ADD NEW DATA -->
+   <!-- Modal -->
+   <div class="modal fade" id="addNewProspect" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Add New Record</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="new_prospect_process.php" method="POST" class="needs-validation" novalidate id="forms">
+        <!-- input data -->
+
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Company Name</label>
+            <input type="text" name="comp_name" required class="form-control" id="formGroupExampleInput" placeholder="">
+                <div class="invalid-feedback">
+                    Please provide a valid Company name.
+                </div>
+        </div>
+
+
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Item Deals</label>
+            <input type="text" name="item_deal" required class="form-control" id="formGroupExampleInput" placeholder="">
+                <div class="invalid-feedback">
+                    Please provide a valid deals.
+                </div>
+        </div>
+
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Status</label>
+        <select name="status" class="form-select form-select-lg form-control" aria-label="Large select example">
+        <option >Prospecting</option>
+        <option >Make contact</option>
+        <option >Qualify your prospect</option>
+        <option >Nurture your prospect</option>
+        <option >Present your offer</option>
+        <option >Overcome objections</option>
+        <option >Close the sales</option>
+        </select>
+        </div>
+
+
+      
+
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Remarks</label>
+            <input type="text" name="remark" required class="form-control" id="formGroupExampleInput" placeholder="">
+                <!-- <div class="invalid-feedback">
+                    Please provide a valid name.
+                </div> -->
+        </div>
+
+        <!-- <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">PDF Link</label>
+            <input type="text" name="pdf" required class="form-control" id="formGroupExampleInput" placeholder="">
+                <div class="invalid-feedback">
+                    Please provide a valid PDF.
+                </div>
+        </div> -->
+
+
+        <div class="mb-3">
+        <label for="formFile" class="form-label">Input PDF File</label>
+        <input class="form-control" name="pdf" type="file" id="formFile">
+        </div>
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="submit" class="btn btn-primary" name="savedata">Submit</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+  <!-- END OF ADD NEW DATA -->
+
+
+
+
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2021</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="logout.php">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 
@@ -74,6 +207,54 @@
 </script>
 
 
+
+
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
+
+
+    <script>
+
+// for form validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+'use strict'
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.from(forms).forEach(form => {
+form.addEventListener('submit', event => {
+  if (!form.checkValidity()) {
+    event.preventDefault()
+    event.stopPropagation()
+  }
+
+  form.classList.add('was-validated')
+}, false)
+})
+})()
+// end of form validation
+
+
+</script>
 
 </body>
 </html>
