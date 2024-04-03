db9b1546110b6c7b644ee964a01ef2f6b51a0326
Junmar <junmarr30@gmail.com>
2024-04-03 22:26:16 +0800

    add border on table client

--- new_prospect.php
+++ new_prospect.php
@@ -15,7 +15,7 @@
   <div class="ml-64">
     <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <p class="text-2xl font-bold mb-4">New Prospect</p>
-      <div class="relative ">
+      <!-- <div class="relative "> -->
       <!-- <div class="absolute top-0 right-10 h-16 w-16 flex items-center justify-center">
         <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
         Add prospect
