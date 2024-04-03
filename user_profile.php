fe6965ab159faba4571a8214a7202cd1e7b0cc56
jcmoralba <106864158+jcmoralba@users.noreply.github.com>
2024-04-03 16:52:10 +0800

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -7,8 +7,13 @@
 </head>
 <body>
     
+<div class="mt-10 ml-64">
 <?php 
 include("sidebar.php");
+?>
+</div>
+
+<?php 
 include("navbar-TEST.php");
 ?>
 
@@ -19,9 +24,16 @@ include("navbar-TEST.php");
   <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
 </svg>
 
+
+
+
+<div class="mb-6">
+    <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Address</label>
+    <input type="text" id="default-input" class="bg-white border border-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 </div>
 
 
-
+    <!--  end div class mt-10 ml-64"> -->
+    </div>
 </body>
 </html>
