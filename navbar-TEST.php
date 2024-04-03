34f1dedfbfe9b625bcbaff8f64a89c42dd5d5ad0
jcmoralba <106864158+jcmoralba@users.noreply.github.com>
2024-04-03 15:51:27 +0800

    idk

--- navbar-TEST.php
+++ navbar-TEST.php
@@ -107,7 +107,7 @@
                     </div>
                     <ul class="py-1 text-gray-500 dark:text-gray-400" aria-labelledby="dropdown">
                         <li>
-                            <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">My profile</a>
+                            <a href="user_profile.php" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">My profile</a>
                         </li>
                         <li>
                             <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Account settings</a>
