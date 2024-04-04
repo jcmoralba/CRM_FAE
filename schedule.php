48500eedb9375001c3630ef0c7dfd862300a65df
Junmar Rosario <junmarr30@gmail.com>
2024-04-04 16:00:39 +0800

    update schedule

--- schedule.php
+++ schedule.php
@@ -78,3 +78,12 @@
 </body>
 <script src="js/schedule.js"></script>
 </html> 
+
+<!-- add mo to -->
+<!-- CREATE TABLE `calendar_event_master` (
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_name` varchar(255) DEFAULT NULL,
+  `event_start_date` date DEFAULT NULL,
+  `event_end_date` date DEFAULT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1; -->
