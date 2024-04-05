e4058c10efd87ad11925c2f926f24136a50979cd
Junmar Rosario <junmarr30@gmail.com>
2024-04-05 13:35:23 +0800

    update login and register

--- register.php
+++ register.php
@@ -31,7 +31,7 @@
             <div class="grid md:grid-cols-2 md:gap-6">
             <div class="relative z-0 w-full mb-5 group">
               <span class="absolute inset-y-0 left-0 flex items-center pl-2 text-gray-500 dark:text-gray-400">+63</span>
-              <input type="text" name="floating_phone" id="floating_phone" maxlength="14" class="block pl-12 py-2.5 pr-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-black focus:outline-none focus:ring-0 focus:border-black peer" placeholder="" required />
+              <input type="text" name="floating_phone" id="floating_phone" maxlength="10" class="block pl-12 py-2.5 pr-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-black focus:outline-none focus:ring-0 focus:border-black peer" placeholder="" required />
               <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-12 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-black peer-focus:dark:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
             </div>
               <div class="relative z-0 w-full mb-5 group">
