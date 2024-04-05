f534ba5d61a11a97b9fb2aafcfa9d1db84a9de6e
Junmar Rosario <junmarr30@gmail.com>
2024-04-05 13:03:26 +0800

    Update new_prospect.php

--- new_prospect.php
+++ new_prospect.php
@@ -35,7 +35,7 @@ include "navbar-TEST.php";
       <p class="text-2xl font-bold mb-4">New Prospect</p>
     </div>
     <div class="relative h-32 w-32 ...">
-      <button data-modal-target="static-modal" data-modal-toggle="static-modal"
+      <button data-modal-target="static-modal" data-modal-toggle="Add prospect"
         class="ml-30 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button">
         Add client
