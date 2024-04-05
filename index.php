68546cf40d7e3f1de5b39c9819f7d18126b8c1f7
jcmoralba <106864158+jcmoralba@users.noreply.github.com>
2024-04-06 00:37:38 +0800

    indexphp

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="ml-64"><h1>hello world</h1></div>
-    
+    <div class="ml-64">
+    <h1>today's total added new prospect/customer/client:0</h1>
+    <h1>todays overall sales:0</h1>
+    </div>
 </body>
 </html>
