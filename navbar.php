10ea7353b9a6e6a0fc4f32ae63819b798ac2b229
jcmoralba <106864158+jcmoralba@users.noreply.github.com>
2024-04-23 09:05:55 +0800

    Revert "Merge branch 'main' into jc-practice-features"
    This reverts commit 9856ec660e41d82cc5855d06c78625b0d5bba6cb, reversing
    changes made to 03c124130239670a889a8a0cb110a9c0140549cc.

--- navbar.php
+++ navbar.php
@@ -9,28 +9,11 @@
   <link rel="stylesheet" href="css/style_index_1.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-  <!-- Font Awesome -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-  rel="stylesheet"
-/>
-<!-- Google Fonts -->
-<link
-  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-  rel="stylesheet"
-/>
-<!-- MDB -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css"
-  rel="stylesheet"
-/>
-
 </head>
 
 <body>
   <div class="main">
     <div class="topbar" style="box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;">
-   
       <div class="toggle">
         <ion-icon name="menu-outline"></ion-icon>
       </div>
@@ -42,77 +25,12 @@
         ?>
       </h5>
       <input id="user1" type="hidden" value="<?php echo  $_SESSION["user"] ?? "noname"; ?>">
-      <div class="d-flex align-items-center">
-      
-      
-      <div class="dropdown">
-        <a
-          data-mdb-dropdown-init
-          class="text-reset me-3 dropdown-toggle hidden-arrow"
-          href="#"
-          id="navbarDropdownMenuLink"
-          role="button"
-          aria-expanded="false"
-        >
-          <i class="fas fa-bell"></i>
-          <span class="badge rounded-pill badge-notification bg-danger">1</span>
-        </a>
-        <ul
-          class="dropdown-menu dropdown-menu-end"
-          aria-labelledby="navbarDropdownMenuLink"
-        >
-          <li>
-            <a class="dropdown-item" href="#">Some news</a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">Another news</a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </li>
-        </ul>
-      </div>
-      <div class="d-flex align-items-center">
-        <div class="dropdown">
-        <a
-          data-mdb-dropdown-init
-          class="dropdown-toggle d-flex align-items-center hidden-arrow"
-          href="#"
-          id="navbarDropdownMenuAvatar"
-          role="button"
-          aria-expanded="false"
-        >
-          <img
-            src="imgs/customer01.jpg"
-            class="rounded-circle"
-            height="25"
-            alt="Black and White Portrait of a Man"
-            loading="lazy"
-          />
-        </a>
-        <ul
-          class="dropdown-menu dropdown-menu-end"
-          aria-labelledby="navbarDropdownMenuAvatar"
-        >
-          <li>
-            <a class="dropdown-item" href="#">My profile</a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">Settings</a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">Logout</a>
-          </li>
-        </ul>
-      </div>
-
-    </div>
+      <div class="user">
+     
+        <img src="imgs/customer01.jpg" alt="">
       </div>
     </div>
-        
 
-<!-- MDB -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.umd.min.js"></script>
 
     <script src="js/main.js"></script>
 
--- sidebar.php
+++ sidebar.php
@@ -12,18 +12,14 @@ require_once("includes/connect.php");
   <title>Document</title>
   <link rel="stylesheet" href="css/style_index_1.css">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </head>
 
 <body>
   <div class="navigation">
-    <img src="img/hytec1.jpg" class="img-fluid opacity-25" alt="">
     <ul>
     <li>
           <a class="navbar-brand" href="">
-          <img src="img/hytecpower1.png" width="80px" height="auto" class="d-inline-block" alt="">
+          <img src="imgs/logo-new.png" width="80px" height="auto" class="d-inline-block" alt="">
             <span class="title">Hytec Power Inc</span>
           </a>
         </li>
