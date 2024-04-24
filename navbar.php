b6425ad3cabb99803e0d939982f1a73cbe19ba2e
Junmar Rosario <junmarr30@gmail.com>
2024-04-24 11:30:03 +0800

    fix navbar

--- navbar.php
+++ navbar.php
@@ -1,4 +1,3 @@
-<?php  ?>
 <!doctype html>
 <html>
 
@@ -9,6 +8,24 @@
   <link rel="stylesheet" href="css/style_index_1.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+  <!-- Font Awesome -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+  <!-- MDB -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css" rel="stylesheet" />
+
+  <style>
+    .notification-profile-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .dropdown {
+      margin-left: 10px; /* Adjust the spacing between notification and profile */
+    }
+  </style>
 </head>
 
 <body>
@@ -18,43 +35,93 @@
         <ion-icon name="menu-outline"></ion-icon>
       </div>
 
-      <h5 >
-        <?php 
-       echo  $_SESSION["user"] ?? "noname";
-        // echo " id:" . $_SESSION["user_id"] ?? '0';
-        ?>
-      </h5>
-      <input id="user1" type="hidden" value="<?php echo  $_SESSION["user"] ?? "noname"; ?>">
-      <div class="user">
-     
-        <img src="imgs/customer01.jpg" alt="">
-      </div>
-    </div>
+      <div class="notification-profile-container">
+        <!-- Notification Dropdown -->
+        <div class="dropdown">
+          <a data-mdb-dropdown-init class="text-reset me-3 dropdown-toggle hidden-arrow" href="#" id="navbarDropdownMenuLink" role="button" aria-expanded="false">
+            <i class="fas fa-bell"></i>
+            <span class="badge rounded-pill badge-notification bg-danger">1</span>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+            <li>
+              <a class="dropdown-item" href="#">Some news</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Another news</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </li>
+          </ul>
+        </div>
 
+        <!-- Avatar -->
+        <div class="dropdown">
+          <a data-mdb-dropdown-init class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#" id="navbarDropdownMenuAvatar" role="button" aria-expanded="false">
+            <img src="imgs/customer01.jpg" class="rounded-circle" height="25" alt="Black and White Portrait of a Man" loading="lazy" />
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+            <li>
+              <a class="dropdown-item" href="#">My profile</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Settings</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#" onclick="logout();">Logout</a>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Name -->
+        <h5>
+          <?php echo $_SESSION["user"] ?? "noname"; ?>
+        </h5>
+      </div>
+
+    </div>
 
     <script src="js/main.js"></script>
 
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <script>
-      function login(){
+      function login() {
 
       }
-let user = document.getElementById("user1").value;
+      let user = document.getElementById("user1").value;
 
-    if (user == "noname") {
-      document.body.innerHTML = ``
-      Swal.fire({
-  icon: "error",
-  title: "Oops...",
-  text: "You need to login first! Redirecting to login page now...",
-  footer: "Something went wrong!"
-}).then((result) => {
-  window.location="login.php";
+      if (user == "noname") {
+        document.body.innerHTML = ``
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "You need to login first! Redirecting to login page now...",
+          footer: "Something went wrong!"
+        }).then((result) => {
+          window.location = "login.php";
+        });
 
-});
-      
-    }
+      }
+    </script>
+
+    <script>
+      function logout() {
+        Swal.fire({
+          title: "Are you sure you want to logout?",
+          text: "",
+          icon: "question",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location = "login_process.php";
+          }
+        });
+
+      }
     </script>
 
 </body>
