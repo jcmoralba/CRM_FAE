11b2ec9927aee6410f0a4d256cf860cfc75ad27c
jcmoralba <106864158+jcmoralba@users.noreply.github.com>
2024-05-07 16:55:12 +0800

    tests

--- includes/connect.php
+++ includes/connect.php
@@ -17,3 +17,28 @@ try {
 }
 
 ?>
+
+
+<?php
+$host1 = "localhost";
+$dbase1 = "test";
+$dsn1 = "mysql:host={$host1};dbname={$dbase1}";
+$uname1 = "root";
+$pw1 = "";
+
+
+date_default_timezone_set("Singapore");
+try {
+    $con1 = new PDO($dsn1, $uname1, $pw1);
+    if($con1) {
+        // echo "connection to database is successful";   
+    }
+}catch (PDOException $err1) {
+    echo $err1->getMessage();
+}
+
+
+
+
+
+?>
--- test3.php
+++ test3.php
@@ -1,89 +1,60 @@
+<?php include 'includes/connect.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Multiple Email Input</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.4.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
+
 <body>
+    <form action="test3_process.php" method="get">
+        <table id="example" class="table table-striped-columns" style="width:100%; border:1px solid black;">
+            <thead>
+                <tr class="bg-dark table-bordered border-dark">
+                    <th class="text-white">MODEL ID</th>
+                    <th class="text-white">ITEM NAME</th>
+                    <th class="text-white">DESCRIPTION</th>
+                    <th class="text-white">SPECS</th>
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="email-container">
-                <div class="email-header">
-                    <h3>HYTEC Deals</h3>
-                </div>
-                <div class="form-group">
-                    <label for="inputEmail">Enter Email Addresses:</label>
-                    <div class="input-group">
-                        
-                        <input type="text" class="form-control" id="inputEmail" placeholder="Enter email addresses" aria-label="Enter email addresses" aria-describedby="btnAdd">
-                        <button class="btn btn-primary" type="button" id="btnAdd">Add</button>
-                        
-                    </div>
-                </div>
-                <div id="emailList"></div>
-                <button type="button" class="btn btn-success" id="btnSave">Save Emails</button>
-            </div>
-        </div>
-    </div>
-</div>
+                    <th class="text-white">ACTION</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM torqcomm";
+                $stmt = $con1->prepare($sql);
+                $stmt->execute();
+                while ($row = $stmt->fetch()) {
+                ?>
+                    <tr>
+                        <td>
+                            <p name="model_id">
+                                <?php echo $row['COL 1']; ?>
+                            </p>
+                        </td>
+                        <td>
+                            <?php echo $row['COL 2']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['COL 3']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['COL 4']; ?>
+                        </td>
+                        <td>
+                            <button name="submit" type="submit" class="button">convert</button>
+                        </td>
+                    </tr>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script>
-$(document).ready(function() {
-    $("#btnAdd").click(function() {
-        var emailInput = $("#inputEmail").val().trim();
-        if (emailInput !== "") {
-            var isValid = validateEmail(emailInput);
-            var isValid = true;
-            if (isValid) {
-                var emailTag = '<span class="badge bg-secondary">' + emailInput + '<button type="button" class="btn-close" aria-label="Remove"></button></span>';
-                $("#emailList").append(emailTag);
-                $("#inputEmail").val(""); // Clear input after adding
-            } else {
-                alert("Invalid email address!");
-            }
-        } else {
-            alert("Please enter an email address!");
-        }
-    });
+                <?php } ?>
+            </tbody>
 
-    // Remove email on close button click
-    $(document).on("click", ".btn-close", function() {
-        $(this).parent().remove();
-    });
-
-    $("#btnSave").click(function() {
-        var emails = [];
-        $("#emailList .badge").each(function() {
-            emails.push($(this).text().trim());
-        });
-        // Now you can save the 'emails' array to your database using AJAX or form submission
-        console.log(emails);
-         // Send the email variable to PHP using AJAX
-         $.ajax({
-            type: "POST",
-            url: "test3_process.php", // Path to your PHP script
-            data: { emails: JSON.stringify(emails) }, // Data to send
-            success: function(response) {
-                alert(response); // Display response from PHP script
-            }
-        });
-        alert("Emails saved successfully!");
-    });
-
-    // Function to validate email address
-    function validateEmail(email) {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    }
-});
-</script>
-
+        </table>
+    </form>
 </body>
+
 </html>
