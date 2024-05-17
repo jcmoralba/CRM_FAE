25d01b2a23591629f1435b50a5b64877c8dc559a
jcmoralba <106864158+jcmoralba@users.noreply.github.com>
2024-05-17 15:53:25 +0800

    sweet alert

--- attendance_process.php
+++ attendance_process.php
@@ -65,7 +65,7 @@ require_once 'includes/connect.php';
           background: '#fff url(/images/trees.png)',
           backdrop: `
             rgba(0,0,123,0.4)
-            url(img/sirjohnquibs.jpg)
+            url('https://sweetalert2.github.io/images/nyan-cat.gif')
             left top
             no-repeat
           `
